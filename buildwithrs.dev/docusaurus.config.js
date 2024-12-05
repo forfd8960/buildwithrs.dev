72350df2b5e360b67b6914c13010107ff3017a94
forfd8960 <forfd8960@gmail.com>
2024-12-05 08:20:27 +0800

    config code theme

--- buildwithrs.dev/docusaurus.config.js
+++ buildwithrs.dev/docusaurus.config.js
@@ -40,15 +40,9 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -61,12 +55,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/buildwithrs.jpg",
       navbar: {
         title: "buildwithrs.dev",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/buildwithrs.svg",
         },
         items: [
           {
@@ -112,8 +106,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} buildwithrs.dev, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        theme: prismThemes.oneLight,
+        darkTheme: prismThemes.oneDark,
       },
     }),
 };
